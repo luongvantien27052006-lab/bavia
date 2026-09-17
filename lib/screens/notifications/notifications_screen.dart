7ea3ade93a9e45ed3f8f3d46a022ce46e96801c8
Lương Văn Tiến <luongvantien27052006@gmail.com>
2026-09-17 21:53:11 +0700

    sdgwegwegjfvjgh

--- lib/screens/notifications/notifications_screen.dart
+++ lib/screens/notifications/notifications_screen.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../core/theme/app_colors.dart';
+import '../../core/theme/app_theme.dart';
 import '../../providers/feed_provider.dart';
 import '../orders/order_detail_screen.dart';
 
